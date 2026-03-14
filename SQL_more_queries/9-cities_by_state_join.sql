2ef8d9a2663c52a9c8def4d8a8d0c442ea469926
khllzdseli <princesskhalilzadeh@gmail.com>
2026-03-14 14:24:32 +0400

    Update 9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
-SELECT cities.id, cities.name AS city, states.name AS state
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
