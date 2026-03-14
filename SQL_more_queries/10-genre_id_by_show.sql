532166d86f8345176a4c5c3947aadb45755b90b7
khllzdseli <princesskhalilzadeh@gmail.com>
2026-03-14 15:33:47 +0400

    Update 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,7 @@
 -- Format: tv_shows.title - tv_show_genres.genre_id
 -- Results sorted by tv_shows.title and tv_show_genres.genre_id in ascending order
 
-SELECT t.title, g.genre_id
-FROM tv_shows t
-JOIN tv_show_genres g ON t.id = g.tv_show_id
-ORDER BY t.title ASC, g.genre_id ASC;
+SELECT tv.title, tg.genre_id
+FROM tv_shows AS tv
+JOIN tv_show_genres AS tg ON tv.id = tg.tv_show_id
+ORDER BY tv.title ASC, tg.genre_id ASC;
