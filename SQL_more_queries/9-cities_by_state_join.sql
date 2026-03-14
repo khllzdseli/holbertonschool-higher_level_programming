b98e765dd2db48799b124da669281b3a0fa6aaa4
khllzdseli <princesskhalilzadeh@gmail.com>
2026-03-14 15:30:09 +0400

    Update 9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,9 @@
-SELECT cities.id, cities.name, states.name
-FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+-- 9. Cities by States
+-- List all cities in the database hbtn_0d_usa
+-- Format: cities.id - cities.name - states.name
+-- Results sorted by cities.id in ascending order
+
+SELECT c.id, c.name, s.name
+FROM cities c
+JOIN states s ON c.state_id = s.id
+ORDER BY c.id ASC;
