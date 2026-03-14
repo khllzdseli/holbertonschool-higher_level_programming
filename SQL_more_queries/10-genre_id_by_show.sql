9d59aeb46d5b37bf3df2eba76b1eb3901105436c
khllzdseli <princesskhalilzadeh@gmail.com>
2026-03-14 15:39:06 +0400

    Update 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,8 @@
--- 10. Genre ID by show
--- List all shows that have at least one genre linked
--- Format: tv_shows.title - tv_show_genres.genre_id
--- Results sorted by tv_shows.title and tv_show_genres.genre_id in ascending order
+-- Lists all shows with at least one genre linked
+-- Displays: tv_shows.title - tv_show_genres.genre_id
 
-SELECT tv.title, tg.genre_id
-FROM tv_shows AS tv
-JOIN tv_show_genres AS tg ON tv.id = tg.tv_show_id
-ORDER BY tv.title ASC, tg.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
